663546a4f83d0695d8e9a179d94beb7ef94414e2
Alf-Henning <alfhenning@gmail.com>
2022-09-11 19:09:59 +0200

    testing

---
+++ code.js
@@ -0,0 +1,35 @@
+
+
+
+let numberEntered = "";
+let arrayTest = [];
+
+
+
+
+
+
+const inputElem = document.getElementById("input");
+
+// Looping through all divs and adding listener to change display when a button is clicked
+
+    inputElem.addEventListener('keydown', (event) => {
+        // Showing more than one digit at a time in display
+
+        let name = event.key; 
+        
+        
+        arrayTest.push(name); 
+
+       
+        
+        if (arrayTest.length === 2) {
+             inputElem.setAttribute("inputmode", "numeric")
+            console.log("success");
+        }
+      
+
+    })
+
+
+    
