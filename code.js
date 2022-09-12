8a965dd72a8a395b3bf7b177d1ddc4f3ec1d6121
Alf-Henning <alfhenning@gmail.com>
2022-09-12 20:33:39 +0200

    Added backspace logic

--- code.js
+++ code.js
@@ -2,26 +2,25 @@
 let array = [];
 const inputElem = document.getElementById("input");
 inputElem.addEventListener('keydown', (event) => {
-  
+
     let charPressed = event.key;
 
     const isLetter = /^[a-z]$/i.test(event.key)
 
     if (isLetter) {
         array.push(charPressed);
-       
     }
 
     if (array.length === 2) {
         inputElem.setAttribute("inputmode", "numeric")
-       
     }
+
+  
 
     if (charPressed === "Backspace") {
         inputElem.setAttribute("inputmode", "text")
-    
     }
-    
+
 
 })
 
