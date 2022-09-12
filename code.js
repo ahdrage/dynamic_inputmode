018764a4c81e2e2e4cadd049284306ef9e079a23
Alf-Henning <alfhenning@gmail.com>
2022-09-12 09:02:36 +0200

    Fix

--- code.js
+++ code.js
@@ -13,37 +13,28 @@ const inputElem = document.getElementById("input");
 
 // Looping through all divs and adding listener to change display when a button is clicked
 
-inputElem.addEventListener('keydown', (event) => {
-    // Showing more than one digit at a time in display
+    inputElem.addEventListener('keydown', (event) => {
+        // Showing more than one digit at a time in display
 
-    let name = event.key;
-
-
-    arrayTest.push(name);
-
-  /*   if (name >= "a" && name <= "z") {
-        console.log("hi there");
+        let name = event.key; 
+        console.log(name);
         
+        arrayTest.push(name); 
 
        
-    } */
+        
+        if (arrayTest.length === 2) {
+             inputElem.setAttribute("inputmode", "numeric")
+            console.log("success");
+        }
 
-   
-    if (arrayTest.length === 1) {
-        inputElem.setAttribute("inputmode", "numeric")
-        console.log("success");
+        if (name === "Backspace") {
+            inputElem.setAttribute("inputmode", "text")
+            
+        }
+      
 
-    }
-
-
-
-
-    if (name === "Backspace") {
-        inputElem.setAttribute("inputmode", "email")
-
-    }
+    })
 
 
-})
-
-
+    
