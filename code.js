b871ff13807048c0c122ff7c37430bbc3e6f5b5c
Alf-Henning <alfhenning@gmail.com>
2022-09-12 20:15:04 +0200

    Added regex instead to check for a-z char

--- code.js
+++ code.js
@@ -5,9 +5,11 @@ inputElem.addEventListener('keydown', (event) => {
   
     let charPressed = event.key;
 
-    if (charPressed >= "a" && charPressed <= "z") {
+    const isLetter = /^[a-z]$/i.test(event.key)
+
+    if (isLetter) {
         array.push(charPressed);
-        console.log(array.length);
+       
     }
 
     if (array.length === 2) {
@@ -15,14 +17,35 @@ inputElem.addEventListener('keydown', (event) => {
         console.log("now array has two values");
     }
 
-
-   
+    
+    
 
 })
+
+
+
 
 
 /* if (charPressed === "Backspace") {
     inputElem.setAttribute("inputmode", "text")
 
 }
+
+
+
+ const input = document.querySelector('input');
+
+  let keyCount = 0;
+
+  input.addEventListener('keyup', ({ key }) => {
+     ? keyCount++ : keyCount = 0;
+
+    if (keyCount === 2) {
+      input.type = 'number';
+    }
+  });
+
+
  */
+
+
